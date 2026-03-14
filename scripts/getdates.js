b8f52905544d1a783cd7ff5ca6ba366bced5196c
marlon-souzaa <marlon.souza01@yahoo.com>
2026-03-14 11:10:34 -0300

    ajusting

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,5 @@
-document.getElementById("lastModified").textContent = document.lastModified;
+const year = document.querySelector("#year");
+const lastModified = document.querySelector("#lastModified");
+
+year.textContent = new Date().getFullYear();
+lastModified.textContent = document.lastModified;
